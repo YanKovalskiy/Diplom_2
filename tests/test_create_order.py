4a1e0612fce3163dba535105b75d0f3c5c61a55b
Vladimir Yankovskiy <vyankovskiy@gmail.com>
2024-06-19 17:00:30 +0500

    refact: test_create_order

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -7,9 +7,9 @@ logger = logging.getLogger(__name__)
 class TestCreateOrder:
 
     @allure.title('Создание заказа авторизованным пользователем')
-    def test_create_order_authorized_user(self, new_user, user_endpoints, order_endpoints, payload_for_create_order):
+    def test_create_order_authorized_user(self, new_user, order_endpoints, payload_for_create_order):
         logger.info(f'+=test_create_order_authorized_user=+')
-        headers = {"Authorization": user_endpoints.access_token}
+        headers = {"Authorization": new_user['access_token']}
         order_endpoints.create_order(headers, payload_for_create_order)
         order_endpoints.check_status_code_is_(200)
         order_endpoints.check_response_field_success_is_(True)
@@ -23,18 +23,18 @@ class TestCreateOrder:
         order_endpoints.check_response_field_success_is_(True)
 
     @allure.title('Создание заказа без ингредиентов авторизованным пользователем')
-    def test_create_order_without_ingredients(self, new_user, user_endpoints, order_endpoints):
+    def test_create_order_without_ingredients(self, new_user, order_endpoints):
         logger.info(f'+=test_create_order_without_ingredients=+')
-        headers = {"Authorization": user_endpoints.access_token}
+        headers = {"Authorization": new_user['access_token']}
         order_endpoints.create_order(headers, None)
         order_endpoints.check_status_code_is_(400)
         order_endpoints.check_response_field_success_is_(False)
         order_endpoints.check_response_field_message_is_('Ingredient ids must be provided')
 
     @allure.title('Создание заказа с некорректными хеш ингредиентов авторизованным пользователем')
-    def test_create_order_with_incorrect_hash_ingredients(self, new_user, user_endpoints, order_endpoints):
+    def test_create_order_with_incorrect_hash_ingredients(self, new_user, order_endpoints):
         logger.info(f'+=test_create_order_with_incorrect_hash_ingredients=+')
-        headers = {"Authorization": user_endpoints.access_token}
+        headers = {"Authorization": new_user['access_token']}
         payload = {
             "ingredients": ["60d3b41abdacab0026a733c6", "609646e4dc916e00276b2870"]
         }
